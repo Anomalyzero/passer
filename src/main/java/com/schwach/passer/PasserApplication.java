95168249423899469e534f3490f3914a6cff458e
Anomalyzero <schwach.alx@gmail.com>
2020-04-13 17:04:55 -0600

    Init

---
+++ src/main/java/com/schwach/passer/PasserApplication.java
@@ -0,0 +1,13 @@
+package com.schwach.passer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schwach/passer/PasserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schwach.passer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
